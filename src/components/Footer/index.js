1b515379155e96aee5ff9c2c07dd4aac7fd2ce65
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-20 11:33:12 -0500

    footer added and implemented in App()

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const copyRightCode = String.fromCharCode(169);
+function Footer() {
+    return (
+        <footer>{copyRightCode}2022 Created by <a href="https://www.github.com/hoganrsherrow">Hogan</a></footer>
+    );
+}
+
+export default Footer;
