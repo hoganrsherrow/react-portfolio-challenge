07ea77d3655578f8b6f87521ee66475a50ff76a7
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-22 17:32:03 -0500

    added react Router to navigate site

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import Footer from './components/Footer';
 import About from './components/About';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hogan's React Portfolio</h1>
-        <nav>
-
-        </nav>
-      </header>
-      <main>
-        <About />
-        <Contact />
-        <Resume />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <header>
+          <h1>Hogan's React Portfolio</h1>
+          <nav>
+            <div><a href="/about">About</a></div>
+            <div><a href="/contact">Contact</a></div>
+            <div><a href="/portfolio">Portfiolio</a></div>
+            <div><a href="/resume">Resume</a></div>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
